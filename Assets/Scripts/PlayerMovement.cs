8cb72cdf84f98554c297cef6371234a142bdb0f4
døffer <christoffer.b.thomsen@gmail.com>
2024-01-03 02:08:08 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Vector2 movement;
+    private Rigidbody2D myBody;
+    private Animator myAnimator;
+
+    [SerializeField] private int speed = 4;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>(); 
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0) 
+        {
+
+            myAnimator.SetFloat ("x", movement.x);
+            myAnimator.SetFloat ("y", movement.y);
+
+                myAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        myBody.velocity = movement * speed;
+    }
+}
